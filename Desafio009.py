da930008e556e24cf4224e680b8b7ce89ddfb0ce
Raphael Torres <rtcarva@gmail.com>
2023-06-19 16:42:08 -0300

    Create Desafio009.py
    Criação de uma tabuada

---
+++ Desafio009.py
@@ -0,0 +1,32 @@
+print('Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada:')
+
+valor1 = int(input('Digite um numero: '))
+
+vezes1 = valor1*1
+vezes2 = valor1*2
+vezes3 = valor1*3
+vezes4 = valor1*4
+vezes5 = valor1*5
+vezes6 = valor1*6
+vezes7 = valor1*7
+vezes8 = valor1*8
+vezes9 = valor1*9
+vezes10 = valor1*10
+
+print('A tabuada de {0} é: \n{0}x1={1} \n{0}x2={2} \n{0}x3={3} \n{0}x4={4} \n{0}x5={5} \n{0}x6={6} \n{0}x7={7} \n{0}x8={8} \n{0}x9={9} \n{0}x10={10}'.format(valor1, vezes1, vezes2, vezes3, vezes4, vezes5, vezes6, vezes7, vezes8, vezes9, vezes10))
+
+#OU SEGUNDA OPCAO
+
+num1 = int(input('Digite um valor: '))
+
+print('{} x {} = {}' .format(num1, 1, num1*1))
+print('{} x {} = {}' .format(num1, 2, num1*2))
+print('{} x {} = {}' .format(num1, 3, num1*3))
+print('{} x {} = {}' .format(num1, 4, num1*4))
+print('{} x {} = {}' .format(num1, 5, num1*5))
+print('{} x {} = {}' .format(num1, 6, num1*6))
+print('{} x {} = {}' .format(num1, 7, num1*7))
+print('{} x {} = {}' .format(num1, 8, num1*8))
+print('{} x {} = {}' .format(num1, 9, num1*9))
+print('{} x {} = {}' .format(num1, 10, num1*10))
+
